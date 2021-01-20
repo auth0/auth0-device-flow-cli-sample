4c5320ba02d4c11b22313e825d14970cbd6f8a46
Filip Skokan <panva.ip@gmail.com>
2021-01-20 16:45:02 +0100

    src: update the example (#2)
    fixes #1

--- cli.js
+++ cli.js
@@ -1,10 +1,7 @@
-const { Issuer } = require('openid-client');
+const { Issuer, errors: { OPError } } = require('openid-client');
 const prompts = require('prompts');
-const got = require('got');
 const open = require('open');
 const questions = require('./questions');
-
-const wait = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
 (async () => {
   const response = await prompts(questions);
@@ -24,82 +21,57 @@ const wait = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
     id_token_signed_response_alg: 'RS256',
   });
 
-  // Device Authorization Request - https://tools.ietf.org/html/draft-ietf-oauth-device-flow-15#section-3.1
-  const { body: deviceAuthorizationResponse } = await got.post(auth0.device_authorization_endpoint, {
-    json: true, // parse the response as json
-    form: true, // send the request body as application/x-www-form-urlencoded
-    body: { client_id: client.client_id, scope: response.scope, audience: response.audience }, // no client authentication
-  });
+  // Device Authorization Request - https://tools.ietf.org/html/rfc8628#section-3.1
+  const handle = await client.deviceAuthorization({ scope: response.scope, audience: response.audience })
 
-  const { verification_uri_complete, device_code, user_code, expires_in } = deviceAuthorizationResponse;
-  let { interval } = deviceAuthorizationResponse;
+  // Device Authorization Response - https://tools.ietf.org/html/rfc8628#section-3.2
+  const { verification_uri_complete, user_code, expires_in } = handle
 
-  // use the interval specified or default to 5seconds as per the specification
-  // interval
-  //   OPTIONAL.  The minimum amount of time in seconds that the client
-  //   SHOULD wait between polling requests to the token endpoint.  If no
-  //   value is provided, clients MUST use 5 as the default.
-  if (interval) {
-    interval = interval * 1000;
-  } else {
-    interval = 5000;
-  }
-
+  // User Interaction - https://tools.ietf.org/html/rfc8628#section-3.3
   await prompts({
     type: 'invisible',
     message: `Press any key to open up the browser to login or press ctrl-c to abort. You should see the following code: ${user_code}. It expires in ${expires_in % 60 === 0 ? `${expires_in / 60} minutes` : `${expires_in} seconds`}.`,
   });
-
   // opens the verification_uri_complete URL using the system-register handler for web links (browser)
   open(verification_uri_complete);
 
-  let done;
+  // Device Access Token Request - https://tools.ietf.org/html/rfc8628#section-3.4
+  // Device Access Token Response - https://tools.ietf.org/html/rfc8628#section-3.5
   let tokens;
-  let logged;
-
-  while (!done && !tokens) {
-    // Device Access Token Request https://tools.ietf.org/html/draft-ietf-oauth-device-flow-15#section-3.4
-    tokens = await client.grant({
-      grant_type: 'urn:ietf:params:oauth:grant-type:device_code',
-      device_code,
-    }).catch((err) => {
-      switch (err.error) {
-        case 'slow_down': // soft error, should re-try in the specified interval
-        case 'authorization_pending': // soft error, should re-try in the specified interval
-          if (!logged) {
-            console.log('\n\nauthorization pending ...');
-            logged = true
-          }
-          return wait(interval);
-          break;
-        case 'access_denied': // end-user declined the device confirmation prompt, consent or rules failed
-          console.error('\n\ncancelled interaction');
-          done = true;
-          break;
-        case 'expired_token': // end-user did not complete the interaction in time
-          console.error('\n\ndevice flow expired');
-          done = true;
-          break;
-        default:
-          if (err.name === 'OpenIdConnectError') {
-            console.error(`\n\nerror = ${err.error}; error_description = ${err.error_description}`);
-            done = true;
-          } else {
-            throw err;
-          }
-      }
-    });
+  try {
+    tokens = await handle.poll()
+  } catch (err) {
+    switch (err.error) {
+      case 'access_denied': // end-user declined the device confirmation prompt, consent or rules failed
+        console.error('\n\ncancelled interaction');
+        break;
+      case 'expired_token': // end-user did not complete the interaction in time
+        console.error('\n\ndevice flow expired');
+        break;
+      default:
+        if (err instanceof OPError) {
+          console.error(`\n\nerror = ${err.error}; error_description = ${err.error_description}`);
+        } else {
+          throw err;
+        }
+    }
   }
 
   if (tokens) {
+    console.log('\n\nresult tokens', { ...tokens });
+
     // requests without openid scope will not contain an id_token
     if (tokens.id_token) {
-      await client.validateIdToken(tokens); // validate ID Token (mandatory claims and signature)
+      console.log('\n\nID Token Claims', tokens.claims());
     }
 
-    console.log('\n\nresult tokens', { ...tokens });
-    console.log('\n\nID Token Claims', tokens.claims);
-    console.log('\n\nUserInfo response', await client.userinfo(tokens));
+    // try-catching this since resource may have been used and the access token may
+    // not be eligible for accessing the UserInfo Response
+    try {
+      console.log('\n\nUserInfo response', await client.userinfo(tokens));
+    } catch (err) {
+      //
+    }
   }
 })().catch((err) => {
   console.error(err);
